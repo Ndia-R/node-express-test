1e87f5f6c9aae23765239c541860c0d49435eff8
ryoichi511 <ryoichiykt@gmail.com>
2022-11-19 08:20:26 +0900

    add get users

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express();
 
 const PORT = 5000;
 
+const users = [
+  { id: 1, name: "Julia", age: 24 },
+  { id: 2, name: "Paul", age: 48 },
+  { id: 3, name: "Steve", age: 28 },
+];
+
 app.get("/", (req, res) => {
-  res.json({ id: 1, name: "Julia", age: 24 });
+  res.send("test");
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
 app.listen(PORT, () => {
