6ea0707fee29d45ca9d422425f6c4ffaaf9b4e92
ryoichi511 <ryoichiykt@gmail.com>
2022-11-19 15:34:51 +0900

    add post

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = 5000;
 
 const users = [
-  { id: 1, name: "Julia", age: 24 },
-  { id: 2, name: "Paul", age: 48 },
-  { id: 3, name: "Steve", age: 28 },
+  { id: 1, username: "Julia", password: "password", email: "julia@gmail.com" },
+  { id: 2, username: "Paul", password: "password", email: "paul@gmail.com" },
+  { id: 3, username: "Steve", password: "password", email: "steve@gmail.com" },
 ];
 
 app.use(express.json());
@@ -22,6 +22,26 @@ app.get("/users", (req, res) => {
 app.get("/user/:id", (req, res) => {
   const id = req.params.id;
   const user = users.find((user) => user.id === Number(id));
+  res.json(user);
+});
+
+app.post("/sign-up", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const exists = users.find((user) => user.username === username);
+  if (exists) {
+    res.status(409).json({
+      message: "すでにユーザーが存在しています",
+    });
+    return;
+  }
+  const user = {
+    id: Math.max(...users.map((user) => user.id)) + 1,
+    username,
+    password,
+    email: `${username}@gmail.com`,
+  };
+  users.push(user);
   res.json(user);
 });
 
